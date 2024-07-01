4bb187b5223a9e3e08fc5edb64326ca5ea85f449
yuvrajsinghgmx <qomfortzone@gmail.com>
2024-07-02 01:21:50 +0530

    Working on SQLAlchemy

--- server/main.py
+++ server/main.py
@@ -1,5 +1,27 @@
 from fastapi import FastAPI
-app = FastAPI()
-@app.get('/')
-def test():
-    return 'hello yuvraj'
+from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+app= FastAPI ()
+
+DATABASE_URL ='postgresql://postgres:sigma@postgres@localhost:5433/musicapp'
+
+engine =create_engine(DATABASE_URL)
+
+sessionLocal = sessionmaker()
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+
+@app.post('/signup')
+def singup_user(user:UserCreate):
+    print(user.name)
+    print(user.email)
+    print(user.password)
+
+
+    pass
